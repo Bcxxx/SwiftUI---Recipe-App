4f5ee89de29a1ab43368f5fcaa187529a8727670
Peter Mihok <peter.mihok115@gmail.com>
2021-09-27 21:32:57 +0200

    - add suffixes

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -35,18 +35,56 @@ class RecipeModel: ObservableObject {
             numerator /= divisor
             denominator /= divisor
             //Get the whole portion if numerator > denominator
-            if numerator > denominator {
+            if numerator >= denominator {
+                
+                //Calculate whole portions
                 wholePortions = numerator / denominator
+                
+                //Calculate the remainder
                 numerator %= denominator
+                
+                //Assign to portion string
+                portion += String(wholePortions)
             }
             //Express the remainder as a fraction
+            if numerator > 0 {
+                
+                //Assign the remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
         }
         
         
+        if var unit = ingredient.unit {
+            
+            var suffix = ""
+            
+            // IF we need to pluralize
+            if wholePortions > 1 {
+                
+                //Calculate appropiate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                    
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
         
-        
-        return String(targetServings)
-        
+        return portion
     }
     
 }
